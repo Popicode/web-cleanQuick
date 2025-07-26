c0fb04166347d0b329d7e6808b7ebeca6955458d
Abraham <escalonacode65@gmail.com>
2025-07-25 20:57:11 -0400

    Restructuración del HTML entorno al nabvar
    Restructuración del HTML y un uso correcto de las buenas practicas, facilitando la manipulación de elementos.

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+
+
+// //?  Esperamos que el HTML este totalmente cargado para ejecutar
+// document.addEventListener("DOMContentLoaded", function () {
+
+
+//     const menuToggle = document.querySelector(".menu-toggle");
+
+//     const navMenu = document.querySelector(".nav-bar");
+
+//     menuToggle.addEventListener("click", function () {
+//         navMenu.classList.toggle("oculto")
+//     })  //? Con .toggle alternamos el estado de oculto
+
+// })
