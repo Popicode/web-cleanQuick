8f6adee6ff6cdc2c2bd0e6617760556817096561
Abraham <escalonacode65@gmail.com>
2025-12-22 18:32:37 -0300

    feat(server.js): configurar 'server.js' con servidor basico

--- server/server.js
+++ server/server.js
@@ -1,7 +1,34 @@
 import dotenv from 'dotenv';
-import express from 'express'
-import { createServer } from 'http'
-dotenv.config()
-const app = express()
+import express from 'express';
+dotenv.config();
+
+const app = express();
+
+const PORT = Number(process.env.PORT ?? 3000);
+
+app.use(express.json());
+app.use(express.text());
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`Servidor levantado en el puerto ${PORT}`)
+);
+
+
+
+
+
+
+
+
+
+
 
 
