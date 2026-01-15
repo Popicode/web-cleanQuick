acdc94087478a9c109eda0bf8215d66bfdd42850
Abraham <escalonacode65@gmail.com>
2026-01-14 21:29:38 -0300

    feat: agregar validacion de telefono para el formulario

--- server/utils/validateContact.js
+++ server/utils/validateContact.js
@@ -10,6 +10,8 @@ export function validateContact(payload) {
     const tel = (payload?.tel ?? "").trim();
     const servicios = (payload?.servicios ?? "").trim();
 
+
+
     if (!name) errors.push('El nombre es obligatorio')
     else if (name.length < 3) errors.push('El nombre tiene que tener como minimo 3 caracteres');
     else if (name.length > 60) errors.push('El nombre no puede tener más de 60 caracteres');
@@ -17,8 +19,9 @@ export function validateContact(payload) {
     if (!email) errors.push('El correo es obligatorio');
     else if (!isValidEmail(email)) errors.push('El correo no es valido');
 
+    if (!tel) errors.push('El telefono es obligatorio')
+    else if (tel.length === 0) errors.push('El campo no puede estar vacio')
 
-    // el tel lo tengo que validar tmb
 
     if (!servicios) errors.push('Debes seleccionar un servicio');
     else if (!servicios_validos.includes(servicios)) errors.push('El servicio seleccionado no es valido');
