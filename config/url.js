74e853c7da2f50d82063fc49c7d32ae108d99137
Abraham <escalonacode65@gmail.com>
2026-01-28 23:42:25 -0300

    feat: agregar url dinamica para el fecth

---
+++ config/url.js
@@ -0,0 +1,14 @@
+const API_BASE_URL_LOCAL = "http://localhost:3000";
+const API_BASE_URL_PROD = "";
+
+let API_URL = '';
+
+let hostName = window.location.hostname;
+
+if (hostName === 'localhost' || hostName === '127.0.0.1') {
+    API_URL = API_BASE_URL_LOCAL
+} else {
+    API_URL = API_BASE_URL_PROD
+}
+
+export default API_URL
